8051a139fb8b496abf651bbf0e39f8a14c7ecf8c
OlenaHlu <glushenkova_es@ukr.net>
2025-03-11 23:41:39 +0200

    start log reg pages

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import logo from "../../assets/logo.svg";
+import css from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} />
+      <div></div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,3 +1,5 @@
+import Header from "../../components/Header/Header";
+
 const MainPage = () => {
   return <h1>Main Page</h1>;
 };
