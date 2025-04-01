5b17edd1b2932e560afd46eb5d9bde9f0afe0eac
OlenaHlu <glushenkova_es@ukr.net>
2025-04-01 23:10:03 +0300

    added to recommended component

---
+++ src/components/Recommended/RecomCard/RecomCard.tsx
@@ -0,0 +1,5 @@
+import css from "./RecomCard.module.css";
+
+const RecomCard = () => {};
+
+export default RecomCard;
--- src/components/Recommended/Recommended.tsx
+++ src/components/Recommended/Recommended.tsx
@@ -1,9 +1,21 @@
 import css from "./Recommended.module.css";
 
+import Icon from "../common/Icon";
+
 const Recommended = () => {
   return (
     <div className={css.recomContainer}>
-      <div></div>
+      <div className={css.topPart}>
+        <h2 className={css.title}>Recommended</h2>
+        <div className={css.paginationBlock}>
+          <button type="button" className={css.btn}>
+            <Icon className={css.iconLeft} iconName="chevron" />
+          </button>
+          <button type="button" className={css.btn}>
+            <Icon className={css.iconRight} iconName="chevron" />
+          </button>
+        </div>
+      </div>
       <div></div>
     </div>
   );
