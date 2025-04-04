ab953e3aff53c4461fed9e0d315b28378d3da4c9
OlenaHlu <glushenkova_es@ukr.net>
2025-04-04 21:32:16 +0300

    user modal done

--- src/components/Modals/UserModal/UserModal.tsx
+++ src/components/Modals/UserModal/UserModal.tsx
@@ -1,22 +1,39 @@
 import css from "./UserModal.module.css";
 
-import Navigation from "../../Header/Navigation/Navigation";
 import Icon from "../../common/Icon";
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
 
 type userModalProps = {
   closeModal: () => void;
 };
 
 const UserModal = ({ closeModal }: userModalProps) => {
+  function getClassActiveLink({ isActive }: { isActive: boolean }) {
+    return clsx(css.link, isActive && css.active);
+  }
   return (
-    <div className={css.modalContainer}>
-      <button className={css.closeBtn} onClick={closeModal}>
-        <Icon className={css.icon} iconName="close" />
-      </button>
-      <Navigation />
-      <button className={css.logoutBtn} type="submit">
-        Log out
-      </button>
+    <div className={css.modal}>
+      <div className={css.modalContainer}>
+        <button className={css.closeBtn} onClick={closeModal}>
+          <Icon className={css.icon} iconName="close" />
+        </button>
+        <ul className={css.navList}>
+          <li>
+            <NavLink className={getClassActiveLink} to="/recommended">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={getClassActiveLink} to="/library">
+              My library
+            </NavLink>
+          </li>
+        </ul>
+        <button className={css.logoutBtn} type="submit">
+          Log out
+        </button>
+      </div>
     </div>
   );
 };
