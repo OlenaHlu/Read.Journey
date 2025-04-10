8c1f653ba8bfe102f1d7b5bc36dba966f082537c
OlenaHlu <glushenkova_es@ukr.net>
2025-04-10 18:03:32 +0300

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
+// import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 
 const RegisterPage = lazy(() => import("./pages/RegisterPage/RegisterPage"));
 const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
@@ -13,19 +13,19 @@ const ReadingPage = lazy(() => import("./pages/ReadingPage/ReadingPage"));
 
 function App() {
   return (
-    <ErrorBoundary>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={<MainPage />} />
-          <Route path="/recommended" element={<RecommendedPage />} />
-          <Route path="/library" element={<MyLibraryPage />} />
-          <Route path="/reading" element={<ReadingPage />} />
-          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-        </Routes>
-      </Suspense>
-    </ErrorBoundary>
+    // <ErrorBoundary>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/recommended" element={<RecommendedPage />} />
+        <Route path="/library" element={<MyLibraryPage />} />
+        <Route path="/reading" element={<ReadingPage />} />
+        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+      </Routes>
+    </Suspense>
+    // </ErrorBoundary>
   );
 }
 
