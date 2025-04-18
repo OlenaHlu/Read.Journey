a4b3aa66392e298b423c05130f16f310b1104424
OlenaHlu <glushenkova_es@ukr.net>
2025-04-18 23:14:31 +0300

    lib board get pretty :)

--- src/components/Dashboard/MyLibraryBoard/MyLibraryBoard.tsx
+++ src/components/Dashboard/MyLibraryBoard/MyLibraryBoard.tsx
@@ -8,7 +8,7 @@ const MyLibraryBoard = () => {
   return (
     <div className={css.libraryContainer}>
       <div className={css.libraryForm}>
-        <h3 className={css.formTitle}>Filters:</h3>
+        <h3 className={css.formTitle}>Create your library:</h3>
         <form onSubmit={handleSubmit} className={css.filterForm}>
           <div className={css.inputGroup}>
             <label htmlFor="bookTitle" className={css.label}>
@@ -22,7 +22,9 @@ const MyLibraryBoard = () => {
             />
           </div>
           <div className={css.inputGroup}>
-            <label className={css.label}></label>
+            <label htmlFor="author" className={css.label}>
+              The author:
+            </label>
             <input
               type="text"
               id="author"
@@ -31,10 +33,15 @@ const MyLibraryBoard = () => {
             />
           </div>
           <div className={css.inputGroup}>
-            <label htmlFor="author" className={css.label}>
-              The author:
+            <label htmlFor="pages" className={css.label}>
+              Number of pages:
             </label>
-            <input />
+            <input
+              type="text"
+              id="pages"
+              placeholder=" "
+              className={css.inputField}
+            />
           </div>
           <button type="submit" className={css.submitButton}>
             Add book
@@ -44,27 +51,33 @@ const MyLibraryBoard = () => {
       <div className={css.recomPart}>
         <h2 className={css.recomPartTitle}>Recommended books</h2>
         <div className={css.booksContainer}>
-          <ul>
-            <li>
+          <ul className={css.booksList}>
+            <li className={css.booksItem}>
               <div className={css.book1}></div>
-              <p>The Orphanage</p>
+              <p className={css.bookName}>The Orphanage</p>
+              <p className={css.bookAuthor}>Serhiy Zhadan</p>
             </li>
             <li>
               <div className={css.book2}></div>
+              <p className={css.bookName}>Melodіja kavi...</p>
+              <p className={css.bookAuthor}>Natalia Gurnyt...</p>
             </li>
             <li>
               <div className={css.book3}></div>
+              <p className={css.bookName}>SIx doors</p>
+              <p className={css.bookAuthor}>Irene Rozdobu...</p>
             </li>
           </ul>
         </div>
-        <div>
-          <Link to="/recommended" className={css.link}>
+
+        <div className={css.navigate}>
+          <Link to="/recommended" className={css.recomLink}>
             Home
           </Link>
+          <Link to="/login">
+            <Icon iconName="login" className={css.icon} />
+          </Link>
         </div>
-        <Link to="/login" className={css.link}>
-          <Icon iconName="login" className={css.icon} />
-        </Link>
       </div>
     </div>
   );
