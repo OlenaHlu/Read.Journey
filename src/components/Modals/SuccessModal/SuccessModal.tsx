23926fc12da185429417e109d6b6f1274bbe9478
OlenaHlu <glushenkova_es@ukr.net>
2025-05-06 01:43:44 +0300

    styled success modal

--- src/components/Modals/SuccessModal/SuccessModal.tsx
+++ src/components/Modals/SuccessModal/SuccessModal.tsx
@@ -1,5 +1,6 @@
 import css from "./SuccessModal.module.css";
 import Icon from "../../../components/common/Icon";
+import ModalWrapper from "../ModalWrapper/ModalWrapper";
 
 type SuccessModalProps = {
   closeModal: () => void;
@@ -7,19 +8,20 @@ type SuccessModalProps = {
 
 const SuccessModal = ({ closeModal }: SuccessModalProps) => {
   return (
-    <div className={css.successContainer}>
-      <button onClick={closeModal} type="button" className={css.closeBtn}>
-        <Icon iconName="close" className={css.icon} />
-      </button>
-      <div className={css.content}>
+    <ModalWrapper closeModal={closeModal}>
+      <div className={css.successContainer}>
+        <button onClick={closeModal} type="button" className={css.closeBtn}>
+          <Icon iconName="close" className={css.icon} />
+        </button>
         <span className={css.emoji}>👍</span>
         <h2 className={css.title}>Good job</h2>
         <p className={css.text}>
-          Your book is now in <strong>the library!</strong> The joy knows no
+          Your book is now in{" "}
+          <span className={css.different}>the library!</span> The joy knows no
           bounds and now you can start your training.
         </p>
       </div>
-    </div>
+    </ModalWrapper>
   );
 };
 
